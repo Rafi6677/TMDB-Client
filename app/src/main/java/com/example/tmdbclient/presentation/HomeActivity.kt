d97e6c1053ac340aa3126574762b0f5047d3a56e
Rafi6677 <rafalgiemza6677@gmail.com>
2020-10-02 19:18:19 +0200

    Prepared activities for project

---
+++ app/src/main/java/com/example/tmdbclient/presentation/HomeActivity.kt
@@ -0,0 +1,37 @@
+package com.example.tmdbclient.presentation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.tmdbclient.R
+import com.example.tmdbclient.databinding.ActivityHomeBinding
+import com.example.tmdbclient.presentation.artist.ArtistActivity
+import com.example.tmdbclient.presentation.movie.MovieActivity
+import com.example.tmdbclient.presentation.tvshow.TvShowActivity
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        binding.moviesButton.setOnClickListener {
+            val intent = Intent(this, MovieActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvShowsButton.setOnClickListener {
+            val intent = Intent(this, TvShowActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.artistsButton.setOnClickListener {
+            val intent = Intent(this, ArtistActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
